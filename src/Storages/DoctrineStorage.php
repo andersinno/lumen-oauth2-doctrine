fab53ab5d962eb1c14f9425b31fc4afe8dfb84ba
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2016-11-19 01:09:26 +0200

    Move doctrine storage connection from parent repo

---
+++ src/Storages/DoctrineStorage.php
@@ -0,0 +1,24 @@
+<?php namespace Nord\Lumen\OAuth2\Doctrine\Storages;
+
+use Doctrine\ORM\EntityManagerInterface;
+use League\OAuth2\Server\Storage\AbstractStorage;
+
+abstract class DoctrineStorage extends AbstractStorage
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+
+    /**
+     * DoctrineStorage constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+}
